d0c6d2f43708a43a1f328d7fdc92d6cc8eab8a5d
Joshua Mellon <joshuamellon1@gmail.com>
2026-08-10 17:47:23 +0100

    added express to packages changed createMailServer to take new (MailServerConfig) config object

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { MailServer } from "./mail-server/mail-server.js";
+import type { MailServerConfig } from "./types/mail-server.js";
 
 export { MailServer };
 
-export function createMailServer(port = 2525): MailServer {
-    return new MailServer(port);
+export function createMailServer(configOrPort: MailServerConfig | number = 2525): MailServer {
+    return new MailServer(configOrPort);
+}
+
+export function createMailServerApi(port = 2525) {
+    // replace later with express api
 }
